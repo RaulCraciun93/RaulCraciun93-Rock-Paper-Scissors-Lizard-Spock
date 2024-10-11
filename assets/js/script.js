1e3e3da6168c40adbbca829ab0b514e00bc1b233
Raul Craciun <raul.carciun@students.codeinstitute.net>
2024-10-11 22:52:58 +0000

    Add click event listener to capture players choice

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,21 @@
 // Wait for the DOM to finish loading before running the game
 // Get the option elements and add event listeners to them
 document. addEventListener("DOMContentLoaded", function() {
-    console.log("DOM fully loaded!");
+    // Select all game option buttons(rock, paper, scissors, lizard, spock)
+    let options = document.querySelectorAll(".option");
+
+    // Add elent listeners to all buttons
+    for (let option of options) {
+        option.addEventListener("click", function() {
+            let playerChoice = this.getAttribute("data-choice");
+            console.log(`Player chose: ${playerChoice}`);
+        })
+    }
+
+    console.log("DOM fully loaded");
+    // Verify that we selected the correct buttons
+    console.log(options);
 });
+
+
 
