1447e97aed2737f1da04963b91b813456777c07f
Raul Craciun <raul.carciun@students.codeinstitute.net>
2024-10-11 22:15:35 +0000

    Set DOMContentLoaded enet listener for game initialization

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+// Wait for the DOM to finish loading before running the game
+// Get the option elements and add event listeners to them
+document. addEventListener("DOMContentLoaded", function() {
+    console.log("DOM fully loaded!");
+});
+
